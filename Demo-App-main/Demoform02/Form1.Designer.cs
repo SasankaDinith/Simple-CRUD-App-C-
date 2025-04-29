84ded084b5f163d31daa665dc9d2046b209edcf9
SasankaDinith <sasankaranawaka0@gmail.com>
2025-04-28 18:54:32 -0800

    first commit

---
+++ Demo-App-main/Demoform02/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace Demoform02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtid = new System.Windows.Forms.TextBox();
+            this.txtaddress = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.Addbtn = new System.Windows.Forms.Button();
+            this.searchBtn = new System.Windows.Forms.Button();
+            this.delBtn = new System.Windows.Forms.Button();
+            this.updateBtn = new System.Windows.Forms.Button();
+            this.txtsearch = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(319, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(335, 45);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Registration Form";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(63, 272);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Address : ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(63, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Id : ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(63, 198);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Name : ";
+            // 
+            // txtid
+            // 
+            this.txtid.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtid.Location = new System.Drawing.Point(187, 137);
+            this.txtid.Name = "txtid";
+            this.txtid.Size = new System.Drawing.Size(320, 31);
+            this.txtid.TabIndex = 4;
+            // 
+            // txtaddress
+            // 
+            this.txtaddress.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtaddress.Location = new System.Drawing.Point(187, 266);
+            this.txtaddress.Name = "txtaddress";
+            this.txtaddress.Size = new System.Drawing.Size(320, 31);
+            this.txtaddress.TabIndex = 5;
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(187, 198);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(320, 31);
+            this.txtName.TabIndex = 6;
+            // 
+            // Addbtn
+            // 
+            this.Addbtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.Addbtn.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Addbtn.Location = new System.Drawing.Point(56, 362);
+            this.Addbtn.Name = "Addbtn";
+            this.Addbtn.Size = new System.Drawing.Size(128, 47);
+            this.Addbtn.TabIndex = 7;
+            this.Addbtn.Text = "Add";
+            this.Addbtn.UseVisualStyleBackColor = false;
+            this.Addbtn.Click += new System.EventHandler(this.Addbtn_Click);
+            // 
+            // searchBtn
+            // 
+            this.searchBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.searchBtn.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchBtn.Location = new System.Drawing.Point(698, 193);
+            this.searchBtn.Name = "searchBtn";
+            this.searchBtn.Size = new System.Drawing.Size(128, 49);
+            this.searchBtn.TabIndex = 8;
+            this.searchBtn.Text = "Search";
+            this.searchBtn.UseVisualStyleBackColor = false;
+            this.searchBtn.Click += new System.EventHandler(this.searchBtn_Click);
+            // 
+            // delBtn
+            // 
+            this.delBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.delBtn.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.delBtn.Location = new System.Drawing.Point(471, 362);
+            this.delBtn.Name = "delBtn";
+            this.delBtn.Size = new System.Drawing.Size(136, 47);
+            this.delBtn.TabIndex = 10;
+            this.delBtn.Text = "Delete";
+            this.delBtn.UseVisualStyleBackColor = false;
+            this.delBtn.Click += new System.EventHandler(this.delBtn_Click);
+            // 
+            // updateBtn
+            // 
+            this.updateBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.updateBtn.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.updateBtn.Location = new System.Drawing.Point(258, 362);
+            this.updateBtn.Name = "updateBtn";
+            this.updateBtn.Size = new System.Drawing.Size(128, 47);
+            this.updateBtn.TabIndex = 11;
+            this.updateBtn.Text = "Update";
+            this.updateBtn.UseVisualStyleBackColor = false;
+            this.updateBtn.Click += new System.EventHandler(this.updateBtn_Click);
+            // 
+            // txtsearch
+            // 
+            this.txtsearch.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtsearch.Location = new System.Drawing.Point(624, 137);
+            this.txtsearch.Name = "txtsearch";
+            this.txtsearch.Size = new System.Drawing.Size(202, 31);
+            this.txtsearch.TabIndex = 5;
+            this.txtsearch.TextChanged += new System.EventHandler(this.txtsearch_TextChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(47, 441);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(873, 309);
+            this.dataGridView1.TabIndex = 12;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(961, 790);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.updateBtn);
+            this.Controls.Add(this.delBtn);
+            this.Controls.Add(this.searchBtn);
+            this.Controls.Add(this.Addbtn);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtsearch);
+            this.Controls.Add(this.txtaddress);
+            this.Controls.Add(this.txtid);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Registration Form";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtid;
+        private System.Windows.Forms.TextBox txtaddress;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button Addbtn;
+        private System.Windows.Forms.Button searchBtn;
+        private System.Windows.Forms.Button delBtn;
+        private System.Windows.Forms.Button updateBtn;
+        private System.Windows.Forms.TextBox txtsearch;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Demo-App-main/Demoform02/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Demoform02
+{
+    public partial class Form1 : Form
+    {
+
+        SqlConnection conn=new SqlConnection("Data Source=DESKTOP-AORRCTR\\SQLEXPRESS;Initial Catalog=Demoform02;Integrated Security=True;Encrypt=False");
+        SqlDataAdapter adp;
+        SqlCommand cmd;    
+        DataTable dt;
+        
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void load_data()
+        {
+            conn.Open();
+                       
+             cmd = new SqlCommand("Select * from [Details]",conn);
+            adp=new SqlDataAdapter();
+            adp.SelectCommand = cmd;    
+            dt=new DataTable();
+            //dt.Clear();
+            adp.Fill(dt);   
+           dataGridView1.DataSource = dt;  
+            dataGridView1.ReadOnly = true;  
+            conn.Close();
+        }
+
+        private void clear_data()
+        {
+            txtid.Text = txtName.Text = txtaddress.Text = " ";
+        }
+        
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            load_data();
+        }
+
+
+
+        private void parameters()
+        {
+            cmd.Parameters.AddWithValue("Id",txtid.Text);
+            cmd.Parameters.AddWithValue("Name",txtName.Text);
+            cmd.Parameters.AddWithValue("Address",txtaddress.Text);
+        }
+
+        private void Addbtn_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtid.Text) || string.IsNullOrEmpty(txtName.Text) || string.IsNullOrEmpty(txtaddress.Text) )
+            {
+                MessageBox.Show("Please fill all the records","Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                
+            }
+            else
+            {
+                cmd = new SqlCommand("Insert into [Details] values (@ID,@Name,@Address)", conn);
+
+                parameters();
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+                load_data();
+                MessageBox.Show("Data added sucessfully", "MessageBox", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+                clear_data();
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int index;
+            index=e.RowIndex;
+
+            DataGridViewRow selectRow = dataGridView1.Rows[index];
+            
+            txtid.Text = selectRow.Cells[0].Value.ToString();
+            txtName.Text = selectRow.Cells[1].Value.ToString();
+            txtaddress.Text = selectRow.Cells[2].Value.ToString();  
+        }
+
+        private void updateBtn_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(txtid.Text) || string.IsNullOrEmpty(txtName.Text) || string.IsNullOrEmpty(txtaddress.Text))
+            {
+                MessageBox.Show("Please fill the all recods", "MessageBox", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+
+            }
+            else
+            {
+                cmd = new SqlCommand("Update [Details] set Id=@Id,Name=@Name,Address=@Address where Id=@Id",conn);
+                parameters();
+                conn.Open();
+              cmd.ExecuteNonQuery();
+                conn.Close();
+                load_data() ;
+                MessageBox.Show("Data updated sucessfully", "MessageBox", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+                clear_data() ;
+            }
+        }
+
+        private void delBtn_Click(object sender, EventArgs e)
+        {
+            
+                cmd = new SqlCommand("Delete from [Details] where Id=@Id", conn);
+            parameters();
+            conn.Open();
+            int i = cmd.ExecuteNonQuery();  
+            conn.Close();
+            load_data();
+            MessageBox.Show("Data deleted sucessfully", "MessageBox", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+           
+            clear_data();   
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void searchBtn_Click(object sender, EventArgs e)
+        {
+            cmd = new SqlCommand("SELECT * FROM [Details] WHERE Id LIKE @Search + '%'  OR Name LIKE @Search + '%' OR Address Like @Search + '%'", conn);
+            cmd.Parameters.AddWithValue("Search", txtsearch.Text);
+            adp = new SqlDataAdapter();
+            adp.SelectCommand = cmd;
+            dt = new DataTable();
+            dt.Clear();
+            adp.Fill(dt);
+            dataGridView1.DataSource = dt;
+            dataGridView1.ReadOnly = true;
+        }
+
+        private void txtsearch_TextChanged(object sender, EventArgs e)
+        {
+           
+
+        }
+    }
+}
